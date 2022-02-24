1d535a79005597aafcf43ee5048e75c1bc35fa16
Svloex <svetloveexperiment@gmail.com>
2022-02-24 23:49:40 +0300

    any all14

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Books from './components/ListBook/Books';
 import Header from './components/Header/Header';
-import {Routes} from 'react-router-dom'
+//import {Routes} from 'react-router-dom'
 import {Route} from 'react-router-dom'
 import {Link} from 'react-router-dom'
 import Error from './components/Error/Error';
@@ -19,10 +19,10 @@ import Error from './components/Error/Error';
           </div>
       </div>
       <div style={{border:"1px solid black"}}>
-          <Routes>
+          {/* <Routes>
              <Route path={'/'} element={<Books/>}/>
             <Route path={'*'} element={<Error/>}/>
-          </Routes>
+          </Routes> */}
       </div> 
     </div>
   );
