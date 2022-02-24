edc5cd75bca9ec84dbd7cbd0d229f72e11e69393
Svloex <svetloveexperiment@gmail.com>
2022-02-25 00:05:21 +0300

    any all19

--- src/App.js
+++ src/App.js
@@ -11,12 +11,12 @@ import Error from './components/Error/Error';
     <div>
       <div style={{border:"1px solid green"}}>
           <Header /> 
-          <div>
+          {/* <div>
             <p>aaa</p>
-            {/* <Link to={"/error"}>error</Link>
+             <Link to={"/error"}>error</Link>
           <hr />
-          <Link to={"/"}>home</Link>  */}
-          </div>
+          <Link to={"/"}>home</Link>  
+          </div> */}
       </div>
       <div style={{border:"1px solid black"}}>
           {/* <Routes>
